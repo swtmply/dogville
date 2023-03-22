e550552e8bf8fb7699031d11d71d44727a5706f8
swtmply <delosreyesjohnallen@gmail.com>
2023-03-22 13:33:37 +0800

    🎨 style: home layout init

--- app/(content)/home/page.tsx
+++ app/(content)/home/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="border-x border-slate-200 min-h-full">
+      <div className="p-4">
+        <h4>Home</h4>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -1,12 +1,20 @@
 import Navigation from "./Navigation";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const ContentLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex w-screen min-h-screen justify-center bg-slate-100">
-      <main className="min-w-[70%] min-h-screen flex">
+      <main className="min-w-[60%] min-h-screen flex justify-between">
         {/* @ts-expect-error Async Server Component */}
         <Navigation />
-        {children}
+        <div className="max-w-2xl grow min-h-screen">{children}</div>
+        {/* TODO Add aside */}
+        <div className="py-4">
+          <div className="flex w-52 rounded-full bg-slate-200 gap-2 p-4">
+            <MagnifyingGlassIcon className="w-6 h-6" />
+            <span>Search Dogville</span>
+          </div>
+        </div>
       </main>
     </div>
   );
