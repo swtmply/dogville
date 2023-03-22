d664b286cca9cdd456d8e5492d3107a7d2ed2bd0
swtmply <delosreyesjohnallen@gmail.com>
2023-03-22 18:08:15 +0800

    🎨 style: added button and dialog to bark button

--- components/navigation/NavigationItems.tsx
+++ components/navigation/NavigationItems.tsx
@@ -1,7 +1,17 @@
 "use client";
+
 import { navigationItems } from "@/lib/navigation-items";
 import React from "react";
 import NavigationItem from "./NavigationItem";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Textarea } from "../ui/textarea";
+import Button from "../ui/button";
 
 const NavigationItems = () => {
   return (
@@ -10,9 +20,24 @@ const NavigationItems = () => {
         <NavigationItem {...item} key={item.href} />
       ))}
       {/* Bark Button */}
-      <button className="text-center w-full py-4 bg-indigo-600 hover:bg-indigo-700 duration-150 text-white font-semibold rounded-full">
-        Bark
-      </button>
+      <Dialog>
+        <DialogTrigger className="text-center w-full py-4 bg-indigo-600 hover:bg-indigo-700 duration-150 text-white font-semibold rounded-full">
+          Bark
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>What do you bark for?</DialogTitle>
+          </DialogHeader>
+          <Textarea
+            placeholder="What we feeling today?"
+            className="px-4 h-32"
+            maxLength={256}
+          />
+          <Button className="bg-indigo-600 hover:bg-indigo-700 text-white py-4 rounded-full font-semibold">
+            Bark
+          </Button>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  icon: React.ReactNode;
+  icon?: React.ReactNode;
   children: React.ReactNode;
 }
 
